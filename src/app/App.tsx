ea2884f9b9a0024135b8f14b56fe35c970a15d98
Greg Christian <gregpuzzles1@gmail.com>
2026-03-05 13:07:40 -0700

    fix: replace BrowserRouter with HashRouter for improved routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppRouter } from './router'
 import { ThemeProvider } from '../features/theme/ThemeProvider'
 
 export function App() {
   return (
-    <BrowserRouter basename="/beehive_hidato">
+    <HashRouter>
       <ThemeProvider>
         <AppRouter />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
